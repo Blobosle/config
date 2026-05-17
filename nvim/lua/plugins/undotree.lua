290706b9d418c2e0694e045b235d6414493b7284
Benjamin Lobos <blobosle@purdue.edu>
2026-05-17 13:35:45 -0400

    feat: added memory to undotree history

--- nvim/lua/plugins/undotree.lua
+++ nvim/lua/plugins/undotree.lua
@@ -1,10 +1,15 @@
 return {
     {
         "mbbill/undotree",
+        init = function()
+            local undodir = vim.fn.stdpath("state") .. "/undo"
+            vim.fn.mkdir(undodir, "p")
+            vim.opt.undodir = undodir
+            vim.opt.undofile = true
+        end,
         keys = {
             { "<leader>u", vim.cmd.UndotreeToggle, desc = "Toggle Undotree" },
         },
         cmd = { "UndotreeToggle", "UndotreeFocus", "UndotreeHide", "UndotreeShow" },
     },
 }
-
