406f3b8518e63c19abad41e9f1a61ee440581952
Benjamin Lobos <blobosle@purdue.edu>
2026-05-25 14:37:29 -0400

    feat: added flash nvim

---
+++ nvim/lua/plugins/flash.lua
@@ -0,0 +1,13 @@
+return {
+    {
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        ---@type Flash.Config
+        opts = {},
+        keys = {
+            { "m", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+            { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+        },
+    }
+}
+
