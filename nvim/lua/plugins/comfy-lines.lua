b005a9ee474e8da1fc46fd46e23cbba4d2d0356a
Benjamin Lobos <blobosle@purdue.edu>
2026-06-09 20:38:34 -0400

    feat: removed comfy lines with 5

--- nvim/lua/plugins/comfy-lines.lua
+++ nvim/lua/plugins/comfy-lines.lua
@@ -1,5 +1,26 @@
 return {
     {
         'mluders/comfy-line-numbers.nvim',
+        config = function()
+            local labels = {}
+            local digits = { "1", "2", "3", "4" }
+
+            local function add(prefix, remaining)
+                if remaining == 0 then
+                    labels[#labels + 1] = prefix
+                    return
+                end
+
+                for _, digit in ipairs(digits) do
+                    add(prefix .. digit, remaining - 1)
+                end
+            end
+
+            for width = 1, 3 do
+                add("", width)
+            end
+
+            require("comfy-line-numbers").setup({ labels = labels })
+        end,
     },
 }
