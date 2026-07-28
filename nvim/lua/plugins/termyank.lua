0e76897975fb244ae853322bcaf9f8f0812027dc
Benjamin Lobos <blobosle@purdue.edu>
2026-07-28 11:45:27 -0400

    fix: term yank recognizes newlines

---
+++ nvim/lua/plugins/termyank.lua
@@ -0,0 +1,9 @@
+return {
+    "hawknewton/termyank.nvim",
+    commit = "158fe58eecdf456d0b122cf2c9e4bb46e1ce22b4",
+    pin = true,
+    event = { "TermOpen", "TermEnter" },
+    opts = {
+        text_objects_span_lines = true,
+    },
+}
